bf05b8bc59923835671b40577e725b6eded0ccbd
abriscioli <abriscioli@makingsense.com>
2013-01-14 17:14:19 -0300

    Creation of the MvcHelloWorld first test

---
+++ MvcPersonPractice/MvcPersonPractice/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPersonPractice.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        //
+        // GET: HelloWorld/Welcome
+        public string Welcome(string name, int numTime = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTime is: " + numTime);
+        }
+    }
+}
